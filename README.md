3e34c30cec1cb45aae1fe2f5c8cd2ff24c018bd9
MaxenceVacheron <vacheronmaxence@gmail.com>
2020-10-18 17:38:46 +0200

    Initial Commit

--- README.md
+++
@@ -1,6 +0,0 @@
-# banana
-
-Banana will soon bring you the best music listening flow for everyone, on every platform, for free.
-Made entirely with python #pythondev
-
-#STAYTUNED
---
+++ src/index.php
@@ -0,0 +1,15 @@
+<?php echo "Welcome on the server. Time is :"; ?>
+<?php echo date('Y-m-d H:i:s'); ?>
+
+<br>
+
+<?php
+echo '<a href="remotev2/remote.html" target="_blank">Projet en cours : Remote</a>';
+
+?>
+<br>
+<?php
+echo '<a href="database.php" target="_blank">Se connecter à la base de données</a>';
+?>
+
+
